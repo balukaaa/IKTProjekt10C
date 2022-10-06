d565ad55e1dbe1304b77a17d4ba13f783df676a6
kaulicsb <kulics101@gmail.com>
2022-10-06 13:26:06 +0200

    kesz

---
+++ HÉV/app.js
@@ -0,0 +1,10 @@
+function szamol(){
+ var indulo =getElementById("select1").value;
+ var erkezo =getElementById("select2").value;
+ 
+ console.log
+ var eredmeny = indulo-erkezo;
+
+
+ alert(Math.abs(eredmeny));
+}
