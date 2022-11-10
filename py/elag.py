d69b9318662025d30060f4483317eb1482d1e858
kaulicsb <kulics101@gmail.com>
2022-11-10 12:22:22 +0100

    kesz

---
+++ py/elag.py
@@ -0,0 +1,8 @@
+
+valasz = input(str("Jó napod van? "))
+if valasz  == "igen" :
+    print("akkor jo")    
+if valasz == 'nem':
+    print("akkor old meg magad")
+elif valasz != "nem" and valasz != "igen":
+    print("bazdmeg")
