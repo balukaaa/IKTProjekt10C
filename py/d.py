207605c521204f713caab18f413a206f88d565e9
Kaulics Balázs <KaulicsB@KKSZKI.LOCAL>
2022-11-17 12:00:51 +0100

    4.feladat

--- py/d.py
+++ py/d.py
@@ -1,5 +1,25 @@
+list = []
 szam1 = int(input("Adj meg egy számot"))
 szam2 = int(input("Adj meg mégegy számot"))
 szam3 = int(input("Adj meg egy harmadik számot"))
-list1 = szam1, szam2, szam3
-list1.sort()
+list.append(szam1)
+list.append(szam2)
+list.append(szam3)
+
+def kisebb():
+    print(min(list))
+
+kisebb()
+
+list = []
+szam1 = int(input("Adj meg egy számot"))
+szam2 = int(input("Adj meg mégegy számot"))
+szam3 = int(input("Adj meg egy harmadik számot"))
+list.append(szam1)
+list.append(szam2)
+list.append(szam3)
+
+def kisebb():
+    print(max(list))
+
+nagyobb()
--- py/elag.py
+++ py/elag.py
@@ -1,4 +1,4 @@
-
+"""""
 valasz = input(str("Jó napod van? "))
 if valasz  == "igen" :
     print("akkor jo")    
@@ -6,3 +6,22 @@ if valasz == 'nem':
     print("akkor old meg magad")
 elif valasz != "nem" and valasz != "igen":
     print("bazdmeg")
+
+szam = int(input("adj egy szamot "))
+if szam % 2 == 0:
+    print("paros")
+else:
+    print("paratlan")
+"""
+
+
+
+
+import random
+gondolt_szam = random.randint(1,6)
+print('Súgok:', gondolt_szam)
+tipp = int(input("tippelj egy számot"))
+if gondolt_szam == tipp:
+    print("jól tippeltél")
+else: 
+    print("nemjól tippeltél")
